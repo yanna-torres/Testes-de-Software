85ecff9223c6acaf8600a7dbc00380cff8b87f87
Yanna Torres <102867787+yanna-torres@users.noreply.github.com>
2023-05-06 14:04:35 -0300

    Update test.js

--- js-exercise-mocha-palindrome/test/test.js
+++ js-exercise-mocha-palindrome/test/test.js
@@ -3,27 +3,42 @@ var assert = require('assert');
 var isPalindrome = require('../js/palindrome.js');
 
 describe('my first test weee', function(){
-  it.only('should pass this canary test', function(){
+  it('should pass this canary test', function(){
     assert.equal(true, true);
   });
-  
+
   it('should return true for argument mom', function(){
     assert.equal(isPalindrome('mom'), true);
   });
 
-  it('should return true for argument dad');
+  it('should return true for argument dad', function() {
+    assert.equal(isPalindrome('dad'), true);
+  });
 
-  it('should return false for argument dude');
+  it('should return false for argument dude', function() {
+    assert.equal(isPalindrome('dude'), false);
+  });
 
-  it('should return true for argument mom mom');
+  it('should return true for argument mom mom', function() {
+    assert.equal(isPalindrome('mom mom'), true);
+  });
 
-  it('should return false for argument dad mom');
+  it('should return false for argument dad mom', function() {
+    assert.equal(isPalindrome('dad mom'), false);
+  });
 
-  it('should return false for an empty string');
+  it('should return false for an empty string', function() {
+    assert.equal(isPalindrome(''), false);
+  });
 
-  it('should return false for empty string argument (just space)');
+  it('should return false for empty string argument (just space)', function() {
+    assert.equal(isPalindrome(' '), false);
+  });
 
-  it('should throw an exception if argument is missing');
-    
-  
+  it('should throw an exception if argument is missing', function(){
+    // https://stackoverflow.com/questions/14966821/testing-for-errors-thrown-in-mocha
+    assert.throws(isPalindrome, Error, 'Missing Argument');
+  });
+
+
 });
